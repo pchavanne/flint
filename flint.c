a91f955e4fa21a9d0f1c62dfe40838f067eee1e6
Philippe Chavanne <philippe.chavanne@gmail.com>
2017-05-18 10:23:20 +0200

    First commit

--- flint.c
+++ flint.c
@@ -11,6 +11,26 @@
 extern "C" {
 #endif
 
+/* Relevant arithmetic exceptions */
+
+/* Uncomment the following line to work around a bug in numpy */
+/* #define ACQUIRE_GIL */
+
+static void
+set_overflow(void) {
+#ifdef ACQUIRE_GIL
+    /* Need to grab the GIL to dodge a bug in numpy */
+    PyGILState_STATE state = PyGILState_Ensure();
+#endif
+    if (!PyErr_Occurred()) {
+        PyErr_SetString(PyExc_OverflowError,
+                "overflow in rational arithmetic");
+    }
+#ifdef ACQUIRE_GIL
+    PyGILState_Release(state);
+#endif
+}
+
 static void
 set_zero_divide(void) {
 #ifdef ACQUIRE_GIL
@@ -24,6 +44,18 @@ set_zero_divide(void) {
 #ifdef ACQUIRE_GIL
     PyGILState_Release(state);
 #endif
+}
+
+static NPY_INLINE int64_t
+safe_abs64(int64_t x) {
+    if (x>=0) {
+        return x;
+    }
+    int64_t nx = -x;
+    if (nx<0) {
+        set_overflow();
+    }
+    return nx;
 }
 
 static const int64_t DEFAULT_MULTIPLIER = 1000000;
@@ -83,8 +115,63 @@ flint_multiply(flint a, flint b){
 static flint
 flint_divide(flint a, flint b){
     flint div = make_flint();
-    div.int_value = (int64_t)(a.int_value * DEFAULT_MULTIPLIER / b.int_value);
+    if (b.int_value == 0){
+        set_zero_divide():
+    }
+    else{
+        div.int_value = (int64_t)(a.int_value * DEFAULT_MULTIPLIER / b.int_value);
+    }
     return div;
+}
+
+static flint
+flint_abs(flint f){
+    flint abs = make_flint();
+    abs.int_value = safe_abs64(f.int_value);
+    return abs;
+}
+
+static int
+flint_sign(flint f){
+    return f.int_value<0?-1:f.int_value==0?0:1;
+}
+
+static int64_t
+flint_int(flint f){
+    return f.int_value;
+}
+
+static double
+flint_double(flint f){
+    return (double)f.int_value / f.multiplier;
+}
+
+static int
+flint_nonzero(flint f){
+    return f.int_value!=0;
+}
+
+/* Expose flint to Python as a numpy scalar */
+
+typedef struct {
+    PyObject_HEAD;
+    flint f;
+} PyFlint;
+
+static PyTypeObject PyFlint_Type;
+
+static NPY_INLINE int
+PyFlint_Check(PyObject* object) {
+    return PyObject_IsInstance(object,(PyObject*)&PyFlint_Type);
+}
+
+static PyObject*
+PyFlint_FromFlint(flint f) {
+    PyFlint* p = (PyFlint*)PyFlint_Type.tp_alloc(&PyFlint_Type,0);
+    if (p) {
+        p->f = f;
+    }
+    return (PyObject*)p;
 }
 
 
---
+++ test_flint.py
@@ -0,0 +1,52 @@
+# -*- coding: UTF-8 -*-
+import flint
+import numpy as np
+
+v1 = 123.45
+v2 = 678.90
+
+f1 = flint(v1)
+f2 = flint(v2)
+
+another_f1 = flint(v1)
+assert another_f1 == f1
+assert another_f1 is not f1
+
+f_from_f1 = flint(f1)
+assert f_from_f1 == f1
+assert f_from_f1 is not f1
+
+assert float(f1) == v1
+assert int(f1) == v1 * f1.multiplier
+assert str(f1) == str(v1)
+
+v3 = v1 + v2
+f3 = flint(v3)
+assert f3 == f1 + f2
+assert f3 == f1 + v2
+assert f3 == v1 + f2
+
+v4 = v2 - v1
+f4 = flint(v4)  # 555.4499999999999
+f4_true = flint(round(v4, 2))   # 555.45
+assert f4_true == f2 - f1
+assert f4_true == v2 - f1
+assert f4_true == f2 - v1
+
+v5 = v1 * v2
+f5 = flint(v5)
+assert f5 == f1 * f2
+assert f5 == f1 * v2
+assert f5 == v1 * f2
+
+v6 = v2 / v1
+f6 = flint(v6)
+assert f6 == f2 / f1
+assert f6 == v2 / f1
+assert f6 == f2 / v1
+
+
+
+arr = np.asarray([f1, f2], dtype=flint)
+arr.mean()
+
