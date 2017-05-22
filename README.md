04038424ab95bc260522bb3a65c3819844985dd6
Philippe Chavanne <philippe.chavanne@gmail.com>
2017-05-22 10:25:13 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# flint
+ flint
+========
+
 Python and Numpy float type with integer arithmetic
+
+###installation
+
+    pip install git+https://github.com/pchavanne/flint
+
--- flint/__init__.py
+++ flint/__init__.py
@@ -1,9 +1,6 @@
 import numpy as np
 
 import flint
-from flint.info import __doc__
-
-__all__ = ['denominator', 'gcd', 'lcm', 'numerator', 'rational']
 
 if np.__dict__.get('rational') is not None:
     raise RuntimeError('The NumPy package already has a rational type')
